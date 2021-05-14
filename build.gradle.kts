9b6b927e3978cb3d218eabbd255942f33f9137a1
dybek <you@example.com>
2021-05-14 18:27:54 +0200

    Initial source code

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.assertj:assertj-core:3.11.1")
+    testImplementation("org.mockito:mockito-core:3.10.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:3.1.0")
 }
 
 tasks.getByName<Test>("test") {
