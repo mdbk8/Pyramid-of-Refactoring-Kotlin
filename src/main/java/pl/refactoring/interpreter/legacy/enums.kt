b2d88f9e260acaa4248a571129e4f4e0b7cd267a
dybek <you@example.com>
2021-05-16 22:47:44 +0200

    Enumes moved to enums.kt file

---
+++ src/main/java/pl/refactoring/interpreter/legacy/enums.kt
@@ -0,0 +1,17 @@
+package pl.refactoring.interpreter.legacy
+
+/**
+ * Copyright (c) 2020 IT Train Wlodzimierz Krakowski (www.refactoring.pl)
+ * Sources are available only for personal usage by Udemy students of this course.
+ */
+enum class EstateMaterial {
+    BRICK, WOOD, FERROCONCRETE, STONE
+}
+
+enum class EstatePlacement {
+    CITY, TOWN, VILLAGE
+}
+
+enum class EstateType {
+    BUNGALLOW, HOUSE, FLAT, CASTLE
+}
